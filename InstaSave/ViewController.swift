a46a6c43b74bc4f61bb6be9f6d7343435e556772
Vladyslav Yakovlev <yakovlev15@icloud.com>
2019-03-01 00:45:57 +0200

    Initial Commit

---
+++ InstaSave/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaSave
+//
+//  Created by Vladyslav Yakovlev on 3/1/19.
+//  Copyright © 2019 Vladyslav Yakovlev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
